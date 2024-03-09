7851fd2271718410e34f1975c85c6f15d8a3a896
delta <154239532+deltaALT@users.noreply.github.com>
2024-03-09 23:40:09 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 echo This script will install some configs for Hyprland. If something goes wrong I strongly recommend you to refer to the guide on the GitHub repo.
-pause
+
 echo If you have not edited the /etc/pacman.conf file yet, do it now. Enable the core-testing and extra-testing repos.
-pause
+
+echo If it asks you for a password, its just pacman installing the dependencies.
 
 sudo pacman -Syyu git hyprland waybar wofi
 git clone https://github.com/deltaALT/hyprland-dots/
