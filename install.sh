541f057a93f9ae692a963ebeb4dd0d34e3ec7765
delta <154239532+deltaALT@users.noreply.github.com>
2024-03-09 23:38:20 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
+echo This script will install some configs for Hyprland. If something goes wrong I strongly recommend you to refer to the guide on the GitHub repo.
+pause
+echo If you have not edited the /etc/pacman.conf file yet, do it now. Enable the core-testing and extra-testing repos.
+pause
+
+sudo pacman -Syyu git hyprland waybar wofi
+git clone https://github.com/deltaALT/hyprland-dots/
+cd hyprland-dots
 cp -r .config/* $HOME/.config/
-echo Done. If You Got A Access Denied Error Please Run As root.
+echo The script has finished installing the configs. You can reboot your system now.
